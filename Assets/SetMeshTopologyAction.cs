3585fc5465312d9b19848b297e03d192b9e73f5a
1080TI\akira <akirasato@psychic-vr-lab.com>
2019-09-06 01:57:13 +0900

    VRoidモデル、マテリアル調整･モーション選定中

---
+++ Assets/SetMeshTopologyAction.cs
@@ -0,0 +1,27 @@
+﻿#if PLAYMAKER
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions.STYLY
+{
+        [ActionCategory("MeshFilter")]
+        public class SetMeshTopologyAction : FsmStateAction
+        {
+                [RequiredField]
+                public FsmOwnerDefault gameObject;
+                [RequiredField]
+                MeshFilter meshFilter;
+                [RequiredField]
+                [UIHint(UIHint.Variable)]
+                public MeshTopology topology;
+                // Code that runs on entering the state.
+                public override void OnEnter()
+                {   
+                        GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+                        meshFilter = go.GetComponent<MeshFilter>();
+                        meshFilter.mesh.SetIndices(meshFilter.mesh.GetIndices(0), topology, 0);
+                        Finish();
+                }
+        }
+}
+#endif
